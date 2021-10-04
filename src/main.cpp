5f9aa0296bac8493aa9a41647344c72b8773d5d7
BuildTools <unconfigured@null.spigotmc.org>
2021-10-04 21:38:08 +0900

    Wireでi2c(マルチプレクサ対応済み)

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,39 @@
 #include <Arduino.h>
-#include <AsyncDelay.h>
-#include <SoftWire.h>
 #include <Wire.h>
 
-// アドレス指定
+//アドレス指定
 #define S11059_ADDR 0x2A
 #define CONTROL_MSB 0x00
 #define CONTROL_1_LSB 0x89
 #define CONTROL_2_LSB 0x09
 #define SENSOR_REGISTER 0x03
-// しきい値
-// 左右、検出する色、読む色
-// 白は全部多いと白だと検知
-#define L_W_R 5000
-#define L_W_G 5000
-#define L_W_B 5800
-#define R_W_R 5000
-#define R_W_G 5000
-#define R_W_B 5000
-// 黒は全部小さいと黒だと検知
-#define L_B_R 5000
-#define L_B_G 5000
-#define L_B_B 5000
-#define R_B_R 5000
-#define R_B_G 5000
-#define R_B_B 5000
-// 緑は緑のみしきい値より多く、その他がしきい値より少なければ緑だと判定
-#define L_G_R 5000
-#define L_G_G 5000
-#define L_G_B 5000
-#define R_G_R 5000
-#define R_G_G 5000
-#define R_G_B 5000
-// 赤は赤のみしきい値より多く、その他がしきい値より少なければ緑だと判定
-#define L_R_R 5000
-#define L_R_G 5000
-#define L_R_B 5000
-#define R_R_R 5000
-#define R_R_G 5000
-#define R_R_B 5000
+
+//Enum
+enum Color{black,white,green,red,silver};
+
+//int
+int lower, higher, rr, rg, rb, rir, lr, lg, lb, lir;
 
 // カラーセンサー2個に対し2個i2cのチャネルを贅沢に作らないと行けないっぽい
-SoftWire LSWire(A4, A5);
-SoftWire RSWire(SDA, SCL);
-AsyncDelay readInterval;
-void test_sensor_setup() {
+void test_sensor_setup(){
   Wire.begin();
   Wire.beginTransmission(S11059_ADDR);
   Wire.write(CONTROL_MSB);
   Wire.write(CONTROL_1_LSB);
+  Wire.endTransmission();
+  Wire.beginTransmission(S11059_ADDR);
+  Wire.write(CONTROL_MSB);
   Wire.write(CONTROL_2_LSB);
   Wire.endTransmission();
 }
-void test_sensor_loop() {
-  int low, high, r, g, b, ir;
+void test_sensor_loop(){
+  int low,high,r,g,b,ir;
   delay(10);
   Wire.beginTransmission(S11059_ADDR);
   Wire.write(SENSOR_REGISTER);
   Wire.endTransmission();
-  Wire.requestFrom(S11059_ADDR, 8);
-  if (Wire.available()) {
+  Wire.requestFrom(S11059_ADDR,8);
+  if(Wire.available()){
     high = Wire.read();
     low = Wire.read();
     r = high << 8 | low;
@@ -81,46 +55,122 @@ void test_sensor_loop() {
   Serial.print(b);
   Serial.print(",");
   Serial.print(ir);
-  Serial.print('\n');
+  Serial.print('\n');  
 }
+
+//TCA9548A
+void change_i2c_port(int byte){
+  Wire.beginTransmission(0x70);
+  Wire.write(1 << byte);
+  Wire.endTransmission();
+}
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
   // シリアル開始
   Serial.begin(9600);
-  // テスト
-  test_sensor_setup();
-  return;
-  // ソフトウェアi2c開始
-  LSWire.begin();
-  RSWire.begin();
-  // コントロールバイトを指定、スリープ解除
-  LSWire.beginTransmission(S11059_ADDR);
-  LSWire.write(CONTROL_MSB);
-  LSWire.write(CONTROL_1_LSB);
-  LSWire.endTransmission();
-  RSWire.beginTransmission(S11059_ADDR);
-  RSWire.write(CONTROL_MSB);
-  RSWire.write(CONTROL_1_LSB);
-  RSWire.endTransmission();
-  // コントロールバイトを指定、バスリリース
-  LSWire.beginTransmission(S11059_ADDR);
-  LSWire.write(CONTROL_MSB);
-  LSWire.write(CONTROL_2_LSB);
-  LSWire.endTransmission();
-  RSWire.beginTransmission(S11059_ADDR);
-  RSWire.write(CONTROL_MSB);
-  RSWire.write(CONTROL_2_LSB);
-  RSWire.endTransmission();
+  //テスト
+  //test_sensor_setup();
+  //return;
+  //I2Cスタート
+  Wire.begin();
+  change_i2c_port(0);
+  Wire.beginTransmission(S11059_ADDR);
+  Wire.write(CONTROL_MSB);
+  Wire.write(CONTROL_1_LSB);
+  Wire.endTransmission();
+  change_i2c_port(1);
+  Wire.beginTransmission(S11059_ADDR);
+  Wire.write(CONTROL_MSB);
+  Wire.write(CONTROL_1_LSB);
+  Wire.endTransmission();
+  change_i2c_port(0);
+  Wire.beginTransmission(S11059_ADDR);
+  Wire.write(CONTROL_MSB);
+  Wire.write(CONTROL_2_LSB);
+  Wire.endTransmission();
+  change_i2c_port(1);
+  Wire.beginTransmission(S11059_ADDR);
+  Wire.write(CONTROL_MSB);
+  Wire.write(CONTROL_2_LSB);
+  Wire.endTransmission();
+
+}
+
+void color_read(){
+  change_i2c_port(0);
+  Wire.beginTransmission(S11059_ADDR);
+  Wire.write(SENSOR_REGISTER);
+  Wire.endTransmission();
+  change_i2c_port(1);
+  Wire.beginTransmission(S11059_ADDR);
+  Wire.write(SENSOR_REGISTER);
+  Wire.endTransmission();
+  change_i2c_port(0);
+  Wire.requestFrom(S11059_ADDR,8);
+  if(Wire.available()){
+    //right-red
+    higher = Wire.read();
+    lower = Wire.read();
+    rr = higher << 8 | lower;
+    //right-green
+    higher = Wire.read();
+    lower = Wire.read();
+    rg = higher << 8 | lower;
+    //right-blue
+    higher = Wire.read();
+    lower = Wire.read();
+    rb = higher << 8 | lower;
+    //right-ir
+    higher = Wire.read();
+    lower = Wire.read();
+    rir = higher << 8 | lower;
+  }
+  Wire.endTransmission();
+  change_i2c_port(1);
+  Wire.requestFrom(S11059_ADDR,8);
+  if(Wire.available()){
+    //right-red
+    higher = Wire.read();
+    lower = Wire.read();
+    lr = higher << 8 | lower;
+    //right-green
+    higher = Wire.read();
+    lower = Wire.read();
+    lg = higher << 8 | lower;
+    //right-blue
+    higher = Wire.read();
+    lower = Wire.read();
+    lb = higher << 8 | lower;
+    //right-ir
+    higher = Wire.read();
+    lower = Wire.read();
+    lir = higher << 8 | lower;
+  }
+  Wire.endTransmission();
+  Serial.print(lr);
+  Serial.print(',');
+  Serial.print(lg);
+  Serial.print(',');
+  Serial.print(lb);
+  Serial.print(',');
+  Serial.print(lir);
+  Serial.print(',');
+  Serial.print(rr);
+  Serial.print(',');
+  Serial.print(rg);
+  Serial.print(',');
+  Serial.print(rb);
+  Serial.print(',');
+  Serial.print(rir);
+  Serial.print('\n');
 }
 
 void loop() {
   //テスト
-  test_ss_loop();
+  color_read();
+  //test_sensor_loop();
   return;
-  // 変数宣言
+  /*// 変数宣言
   int lower, higher, rr, rg, rb, rir, lr, lg, lb, lir;
   delay(10);
   // 出力データバイトを指定
@@ -173,34 +223,22 @@ void loop() {
     rir = higher << 8 | lower;
   }
   RSWire.endTransmission();
-  // それぞれの値をもとに色を判別
-  // 色は白=0、黒=1、緑=2、赤=3
-  int left = 0;
-  int right = 0;
-  if (lr > L_W_R && lg > L_W_G && lb > L_W_B) {
-    left = 0;
-  }
-  if (rr > R_W_R && rg > R_W_G && rb > R_W_B) {
-    right = 0;
-  }
-  if (lr < L_B_R && lg < L_B_G && lb < L_B_B) {
-    left = 1;
-  }
-  if (rr < R_B_R && rg < R_B_G && rb < R_B_B) {
-    right = 1;
-  }
-  if (lr < L_G_R && lg > L_G_G && lb < L_G_B) {
-    left = 2;
-  }
-  if (rr < R_G_R && rg > R_G_G && rb < R_G_B) {
-    right = 2;
-  }
-  if (lr > L_R_R && lg < L_R_G && lb < L_R_B) {
-    left = 3;
-  }
-  if (rr > R_R_R && rg < R_R_G && rb < R_R_B) {
-    right = 3;
-  }
-  Serial.print(left * 10 + right); //左が10の位、右が1の位
-  Serial.print('\n');
+  // それぞれの値をカンマ区切りで改行までシリアルで送信
+  // 右左それぞれ、赤、緑、青、赤外線と出す
+  Serial.print(lr);
+  Serial.print(',');
+  Serial.print(lg);
+  Serial.print(',');
+  Serial.print(lb);
+  Serial.print(',');
+  Serial.print(lir);
+  Serial.print(',');
+  Serial.print(rr);
+  Serial.print(',');
+  Serial.print(rg);
+  Serial.print(',');
+  Serial.print(rb);
+  Serial.print(',');
+  Serial.print(rir);
+  Serial.print('\n');*/
 }
